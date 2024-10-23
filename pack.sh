3ca7c146cfe7e7ec1fc5f31b343a5b7300984e66
BinL233 <835741923@qq.com>
2024-10-23 16:28:31 +0800

    Updated pack.sh for path problem

--- pack.sh
+++ pack.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Build backend image
-docker buildx build --platform linux/amd64 -t binl2333/binltools_backend:test . 
+cd gin_backend
+sudo docker buildx build --platform linux/amd64 -t binl2333/binltools_backend:test . 
 
 # Build frontend image
-docker buildx build --no-cache --platform linux/amd64 -t binl2333/binltools_web:test . 
+cd ../web
+sudo docker buildx build --no-cache --platform linux/amd64 -t binl2333/binltools_web:test . 
 
 # Save images to .tar file
-docker save -o binltools_backend.tar binl2333/binltools_backend:test
-docker save -o binltools_web.tar binl2333/binltools_web:test
+cd ..
+sudo docker save -o binltools_backend.tar binl2333/binltools_backend:test
+sudo docker save -o binltools_web.tar binl2333/binltools_web:test
 
 # Send files to the server
 scp binltools_backend.tar ubuntu@124.222.134.63:/home/ubuntu/binltools_backend.tar
